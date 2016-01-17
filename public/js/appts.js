f27a03171c49ec4bd00066e5c9682a926da2ba71
Carolina No <carolina.s.no@gmail.com>
2016-01-17 16:36:57 -0500

    renders appts

--- public/js/appts.js
+++ public/js/appts.js
@@ -1,5 +1,32 @@
 ////////////// APPOINTMENTS
 
+function renderAppointments(user){
+  var appointments = user.appointments;
+  var $display = $('#display-appointments');
+  $display.empty();
+  appointments.forEach(function(app){
+    console.log(app);
+    var $appDiv = $('<div id="'+app._id+'">');
+    $appDiv.append( $('<h4>').text(app.date) );
+    $appDiv.append( $('<h4>').text(app.location) );
+    $appDiv.append( $('<h4>').text(app.doctor) );
+    $appDiv.append( $('<h4>').text(app.notes) );
+    $appDiv.append( $('<button data-id="'+app._id+'">').addClass('remove-app').text('Delete App') )
+    $display.append($appDiv);
+  });
+}
+
+function getUserAppts(){
+  $.ajax({
+    method: 'get',
+    url: '/users',
+    success: function(data){
+      console.log(data);
+      renderAppointments(data);
+    }
+  });
+}
+
 function addApps(appData, callback){
   console.log()
   $.ajax({
@@ -7,8 +34,8 @@ function addApps(appData, callback){
     url: '/users/appointments',
     data: {user: appData},
     success: function(){
-      console.log('added the appointment?');
-      callback();
+      // console.log('added the appointment?');
+      getUserAppts();
     }
   })
 }
