52a43dc4f9ee8e2daacfd1af38c26ca2dea2e80d
Brian Duggan <bduggan3387@gmail.com>
2016-01-17 14:15:04 -0500

    adds add medication

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+
+/// GETS THE CURRENT USER'S INFO
 router.get('/', function(req,res){
   console.log('this is the get request' + req.user._id);
   User.findById(req.user._id, function(err, user){
@@ -10,6 +12,7 @@ router.get('/', function(req,res){
   })
 });
 
+// CREATES A USER
 router.post('/', function(req, res){
   var newUser = new User(req.body.user);
   newUser.save(function(err, databaseUser){
@@ -17,6 +20,7 @@ router.post('/', function(req, res){
   });
 });
 
+// LOGS A USER IN AND ASSIGNS A TOKEN
 router.post('/authenticate', function(req, res){
   var username = req.body.username; // What username did THEY type in
   var passwordTry = req.body.password; // What password attempt did THEY type in
@@ -34,6 +38,7 @@ router.post('/authenticate', function(req, res){
   });
 });
 
+// UPDATES THE USER'S PROFILE
 router.patch('/', function (req, res){
   console.log(req.user);
   if (req.user){
@@ -44,32 +49,37 @@ router.patch('/', function (req, res){
   }
 });
 
-router.patch('/', function (req, res){
+// router.patch('/', function (req, res){
+//   if (req.user){
+//     req.user.appointments = req.body.user;
+//     req.user.save(function(err, databaseUser){  // Save the user
+//       res.json(databaseUser); // Send the updated user as JSON
+//     });
+//   }
+// });
+
+// ADDS A NEW MEDICATION TO THE USER
+router.post('/medications', function (req, res){
   if (req.user){
-    req.user.appointments = req.body.user;
+    console.log("this is the user: "+req.user);
+    console.log("this is the body"+req.body.user);
+    console.log("this is the user's medications"+req.user.medications)
+    req.user.medications.push(req.body.user);
+    console.log("this is the new medication"+req.user);
     req.user.save(function(err, databaseUser){  // Save the user
       res.json(databaseUser); // Send the updated user as JSON
     });
   }
 });
 
-router.patch('/', function (req, res){
-  if (req.user){
-    req.user.medications = req.body.user;
-    req.user.save(function(err, databaseUser){  // Save the user
-      res.json(databaseUser); // Send the updated user as JSON
-    });
-  }
-});
-
-router.patch('/', function (req, res){
-  if (req.user){
-    req.user.foodRestrictions = req.body.user;
-    req.user.save(function(err, databaseUser){  // Save the user
-      res.json(databaseUser); // Send the updated user as JSON
-    });
-  }
-});
+// router.patch('/', function (req, res){
+//   if (req.user){
+//     req.user.foodRestrictions = req.body.user;
+//     req.user.save(function(err, databaseUser){  // Save the user
+//       res.json(databaseUser); // Send the updated user as JSON
+//     });
+//   }
+// });
 
 
 
