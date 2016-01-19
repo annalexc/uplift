e28279ebec3f0d367f6a63a50d59909b79138497
Brian Duggan <bduggan3387@gmail.com>
2016-01-19 15:11:29 -0500

    fixes update profile function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -48,14 +48,49 @@ function logOut(){
 //////// RENDER USER ///////
 function renderUserProfile(user){
   console.log(user);
-  var profile = $('#profile');
+  var $profile = $('#display-profile');
   var birthdate = convertTimeToWords(user.profile[0].birthdate);
   console.log(birthdate);
-  profile.empty();
-  profile.append( $('<h2>').text(user.profile[0].firstName + " " + user.profile[0].lastName));
-  profile.append( $('<h4>').text("Birthdate: " + birthdate).addClass('birthdate') );
-  profile.append( $('<h4>').text("Gender: " + user.profile[0].gender).addClass('gender'));
-  profile.append( $('<h4>').text("Contact Number: " + user.profile[0].phoneNum).addClass('phoneNum'));
+  $profile.empty();
+  $profile.append( $('<h2>').text(user.profile[0].firstName + " " + user.profile[0].lastName));
+  $profile.append( $('<h4>').text("Birthdate: " + birthdate).addClass('birthdate') );
+  $profile.append( $('<h4>').text("Gender: " + user.profile[0].gender).addClass('gender'));
+  $profile.append( $('<h4>').text("Contact Number: " + user.profile[0].phoneNum).addClass('phoneNum'));
+
+  var $updateProfile = $('<div id="profile-update-form">');
+  var $updateProfileForm = $('<form method="patch">').addClass('update-profile');
+  var newBirthdate = convertTimeToValue(user.profile[0].birthdate);
+  $updateProfileForm.append($('<h5>').addClass('updates').text('Update Profile'));
+  $updateProfileForm.append($('<label for="firstName">').text('First Name: ') );
+  $updateProfileForm.append($('<input type="text" name="firstName" value="'+user.profile[0].firstName+'">'));
+  $updateProfileForm.append($('<label for="lastName">').text('Last Name: ') );
+  $updateProfileForm.append($('<input type="text" name="lastName" value="'+user.profile[0].lastName+'">'));
+  $updateProfileForm.append($('<label for="birthdate">').text('D.O.B.:') );
+  $updateProfileForm.append($('<input type="date" name="birthdate" value="'+newBirthdate+'">'));
+  $updateProfileForm.append($('<label for="gender">').text('Gender: ') );
+
+  var $genderSelect = $('<select name="gender">');
+  if (user.profile[0].gender === "Not Specified"){
+    $genderSelect.append( $('<option value="Not Specified" selected="selected">').text('Select One') );
+    $genderSelect.append( $('<option value="Male">').text('Male') );
+    $genderSelect.append( $('<option value="Female">').text('Female') );
+  } else if (user.profile[0].gender === "Male") {
+    $genderSelect.append( $('<option value="Not Specified">').text('Select One') );
+    $genderSelect.append( $('<option value="Male" selected="selected">').text('Male') );
+    $genderSelect.append( $('<option value="Female">').text('Female') );
+  } else {
+    $genderSelect.append( $('<option value="Not Specified">').text('Select One') );
+    $genderSelect.append( $('<option value="Male">').text('Male') );
+    $genderSelect.append( $('<option value="Female" selected="selected">').text('Female') );
+  }
+  $updateProfileForm.append($genderSelect);
+
+  $updateProfileForm.append($('<label for="phoneNum">').text('Contact Number: ') );
+  $updateProfileForm.append($('<input type="tel" name="phoneNum" value="'+user.profile[0].phoneNum+'">'));
+  $updateProfileForm.append( $('<button data-id="'+user._id+'">').text( 'Update Profile') );
+
+  $updateProfile.append($updateProfileForm);
+  $profile.append($updateProfile);
 
 
 }
@@ -89,7 +124,7 @@ function updateUser(userData, callback){
 
 //////// UPDATE PROFILE HANDLER ////////
 function updateUserProfileHandler(){
-  $('form#update-profile').on('submit', function(e){
+  $('#display-profile').on('submit', '.update-profile', function(e){
     e.preventDefault();
     var firstNameField = $('input[name="firstName"]');
     var firstName = firstNameField.val();
@@ -101,7 +136,6 @@ function updateUserProfileHandler(){
     var gender = genderField.val();
     var phoneNumField = $('input[name="phoneNum"]');
     var phoneNum = phoneNumField.val();
-    console.log(phoneNum);
     var userProfile = {firstName: firstName, lastName: lastName, birthdate: birthdate, gender: gender, phoneNum: phoneNum};
     updateUser(userProfile, function(){
       getUser();
